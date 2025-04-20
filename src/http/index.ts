cfce652b1a73ad4d70fa2887e80ffed608a7cc96
letengzz <2020885569@qq.com>
2025-04-20 20:11:27 +0800

    chore: update http

--- src/http/index.ts
+++ src/http/index.ts
@@ -7,7 +7,7 @@ import type {
 } from 'axios'
 
 import axios from 'axios'
-// import { ElMessage } from "element-plus";
+import { ElMessage } from 'element-plus'
 
 // 定义返回值类型
 interface Result<T> {
@@ -56,7 +56,7 @@ class Http {
           return res.data
         }
         else {
-          // ElMessage.error(res.data.msg || "服务器出错");
+          ElMessage.error(res.data.msg || '服务器出错')
           return Promise.reject(res.data.msg || '服务器出错')
         }
       },
@@ -67,60 +67,60 @@ class Http {
           switch (error.response.status) {
             case 400:
               error.data.msg = '错误请求'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 401:
               error.data.msg = '未授权,请重新登录'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 403:
               error.data.msg = '拒绝访问'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 404:
               error.data.msg = '请求错误,未找到接口'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 405:
               error.data.msg = '请求方法未允许'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 408:
               error.data.msg = '请求超时'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 500:
               error.data.msg = '服务器端出错'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 501:
               error.data.msg = '网络未实现'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 502:
               error.data.msg = '网络错误'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 503:
               error.data.msg = '服务不可用'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 504:
               error.data.msg = '网络超时'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             case 505:
               error.data.msg = 'http版本不支持该请求'
-              // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
               break
             default:
               error.data.msg = `连接错误${error.reponse.message}`
-            // ElMessage.error(error.data.msg);
+              ElMessage.error(error.data.msg)
           }
         }
         else {
           error.data.msg = '连接到服务器失败'
-          // ElMessage.error(error.data.msg);
+          ElMessage.error(error.data.msg)
         }
         return Promise.reject(error)
       },
