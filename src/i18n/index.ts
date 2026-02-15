8d0ce4e1c40301aaa13ad6a30a2252f2f4647bbd
letengzz <2020885569@qq.com>
2026-02-15 18:14:34 +0800

    feat(i18n): 实现国际化模块动态加载和按需路由翻译

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,41 @@
+
 import type { App } from 'vue'
 import { createI18n } from 'vue-i18n'
-import zhCN from './locales/zh-CN'
-import enUS from './locales/en-US'
+/**
+ * 获取浏览器的语言
+ */
+const getBrowserLanguage = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const browserLang = navigator.language || (navigator as any).userLanguage
+  return ['zh-CN', 'en-US'].includes(browserLang) ? browserLang : 'zh-CN'
+}
+
+const currentLang = localStorage.getItem('locale') || getBrowserLanguage()
 
 // 创建 i18n 实例
 const i18n = createI18n({
   legacy: false, // 使用 Composition API
-  locale: 'zh-CN',
+  locale: currentLang,
   fallbackLocale: 'zh-CN', // fallback 语言
-  messages: {
-    'zh-CN': zhCN,
-    'en-US': enUS,
-  },
+  messages: {},
 })
+
+/**
+ * 动态加载语言包
+ */
+export async function loadLanguage(lang: string) {
+  const messages = await import(`./locales/${lang}.ts`).then((messages) => {
+    i18n.global.setLocaleMessage(lang, messages.default)
+    localStorage.setItem('locale', lang)
+    return lang
+  });
+  return messages
+}
+
+loadLanguage(currentLang)
 
 export const useI18n = (app: App) => {
   app.use(i18n)
 }
+
+export default i18n
