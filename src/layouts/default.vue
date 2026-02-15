e0ab27b71f123280140bef248243369c98db230a
letengzz <2020885569@qq.com>
2026-02-15 13:51:21 +0800

    feat: 添加Sass依赖和UnoCSS导航链接

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,6 +5,7 @@
       <router-link to="/demo" class="item">demo</router-link>&nbsp;
       <router-link to="/vueuse" class="item">vueuse</router-link>&nbsp;
       <router-link to="/i18n" class="item">i18n</router-link>&nbsp;
+      <router-link to="/uno" class="item">UnoCSS</router-link>&nbsp;
     </header>
     <router-view></router-view>
   </div>
