b2f40ab3c1d18b45820ae0f15d7b46038dc874ad
letengzz <2020885569@qq.com>
2026-06-11 14:44:49 +0800

    feat: 引入shadcn vue组件库并添加基础按钮组件

---
+++ src/pages/shadcn.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ArrowUpIcon } from '@lucide/vue'
+</script>
+
+<template>
+  <div class="flex flex-wrap items-center gap-2 md:flex-row">
+    <Button variant="outline">
+      Button
+    </Button>
+    <Button variant="outline" size="icon" aria-label="Submit">
+      <ArrowUpIcon />
+    </Button>
+  </div>
+</template>
